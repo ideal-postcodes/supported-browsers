c5ad9e20d440b134395132974b3c9af14eacf10d
Christopher Blanchard <cablanchard@gmail.com>
2019-10-14 14:36:46 +0000

    feat(CBT): Export CBT launcher helper
    Lint

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import { browsers } from "../lib/index";
+import { browsers, toCbtLaunchers } from "../lib/index";
 
 describe("Support matrix", () => {
   it("exports a list of browsers", () => {
@@ -8,5 +8,81 @@ describe("Support matrix", () => {
     assert.isTrue(Object.values(browsers).some(b => b.mobile));
     assert.isTrue(Object.values(browsers).some(b => b.legacy));
     assert.isTrue(Object.values(browsers).some(b => b.latest));
+  });
+});
+
+describe("toCbtLaunchers", () => {
+  const matrix = {
+    foo: {
+      browserName: "brow",
+      deviceName: "dev",
+      platformVersion: "5.0",
+      platformName: "Android",
+      deviceOrientation: "portrait",
+      mobile: true,
+      legacy: true,
+    },
+    bar: {
+      browserName: "desktopBrowser",
+      version: "77",
+      platform: "os",
+      desktop: true,
+      latest: true,
+    },
+    baz: {
+      browserName: "baz",
+      desktop: true,
+      latest: true,
+    },
+    quux: {
+      browserName: "quux",
+      mobile: true,
+      legacy: true,
+    },
+  };
+
+  it("returns correctly formatted launchers", () => {
+    const build = "some-build-123";
+    const name = "project";
+    assert.deepEqual(toCbtLaunchers(matrix, { name, build }), {
+      foo: {
+        browserName: "brow",
+        deviceName: "dev",
+        platformVersion: "5.0",
+        platformName: "Android",
+        deviceOrientation: "portrait",
+        base: "CrossBrowserTesting",
+        name,
+        build,
+      },
+      bar: {
+        browserName: "desktopBrowser",
+        version: "77",
+        platform: "os",
+        name,
+        build,
+        base: "CrossBrowserTesting",
+        screenResolution: "1366x768",
+      },
+      baz: {
+        browserName: "baz",
+        version: "",
+        platform: "",
+        name,
+        build,
+        base: "CrossBrowserTesting",
+        screenResolution: "1366x768",
+      },
+      quux: {
+        browserName: "quux",
+        deviceName: "",
+        platformVersion: "",
+        platformName: "",
+        deviceOrientation: "",
+        base: "CrossBrowserTesting",
+        name,
+        build,
+      },
+    });
   });
 });
