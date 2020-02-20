4678f531a908bad456b915816d9ec0c5277915af
Christopher Blanchard <cablanchard@gmail.com>
2020-02-20 12:01:57 +0000

    feat(Util): Add util functions to generate sauce config

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,5 @@
+import { execSync } from "child_process";
+
 export type SupportMatrix = Record<string, Browser>;
 
 export type Browser = MobileBrowser | DesktopBrowser;
@@ -119,3 +121,59 @@ export const browsers: SupportMatrix = {
   ...latestMobile,
   ...legacyMobile,
 };
+
+/**
+ * Return true if CI environment (Github actions) detected
+ */
+export const ci = (): boolean => process.env.GITHUB_ACTION !== undefined;
+
+/**
+ * Retrieve git sha
+ */
+export const gitSha = (): string =>
+  execSync("git rev-parse --short HEAD")
+    .toString()
+    .trim();
+
+/**
+ * Build ID
+ *
+ * Use gitsha and date if local
+ *
+ * Use Github action ID if CI
+ */
+export const generateBuild = (): string =>
+  ci() ? `${process.env.GITHUB_RUN_ID}` : `${gitSha()}-${new Date().toJSON()}`;
+
+interface Options {
+  defaults?: any;
+  testName: string;
+  build?: string;
+}
+
+/**
+ * Generates sauce config
+ */
+export const config = ({ testName, build, defaults = {} }: Options) => ({
+  ...defaults,
+  reporters: ["dots", "karma-typescript", "saucelabs"],
+  plugins: [
+    "karma-mocha",
+    "karma-typescript",
+    "karma-polyfill",
+    "karma-sauce-launcher",
+  ],
+  browserDisconnectTimeout: 10000,
+  browserDisconnectTolerance: 2,
+  browserNoActivityTimeout: 30000,
+  captureTimeout: 120000,
+  transports: ["websocket", "polling"],
+  sauceLabs: {
+    testName,
+    startConnect: true,
+    build: build === undefined ? generateBuild() : build,
+    recordVideo: false,
+    recordScreenshots: false,
+    public: "public restricted",
+  },
+});
